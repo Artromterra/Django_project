a8e088dd6c1ec78d46cd8a42ef8bde4d834c1c54
dmitriy <oberon@mail.ru>
2025-02-02 17:20:45 +0300

    Remove subcategory model

--- online_store/shop/admin.py
+++ online_store/shop/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models.product import Product
-from .models.category import Category, SubCategory
+from .models import Product, Category
 
 
 admin.site.register(Product)
 admin.site.register(Category)
-admin.site.register(SubCategory)
+
 
