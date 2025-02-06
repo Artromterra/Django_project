f19dfec90591152ec3a14b1225c11af6d581af7e
Роман Лавдоренко <wertuozo@yandex.ru>
2025-02-06 21:23:02 +0300

    add html code for footer and header

--- online_store/shop/views.py
+++ online_store/shop/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # TODO: Remove the check_integration_with_frontend view function
 #  - it is needed to check integration with the base frontend
 def check_integration_with_frontend(request: HttpRequest) -> HttpResponse:
-    return render(request, "about.html")
+    return render(request, "base.html")
