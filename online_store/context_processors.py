be340ad6ca78e60393c40b3e9e3250c97ea0771f
Владимир Изволенский <izvolenskiy92@mail.ru>
2025-02-09 01:20:05 +0300

    Revert "1)Создан файл category_menu.py в котором описан сервис для возврата активных..."
    This reverts commit 0ebd0ff35d56e075f059fe983ab1d606ffd6c484

--- online_store/context_processors.py
+++
@@ -1,9 +0,0 @@
-from services.category_menu import CategoryMenuService
-
-def category_menu():
-    """
-    Context processor, который добавляет меню категорий в контекст всех шаблонов.
-    """
-    return {
-        'category_menu': CategoryMenuService.get_cached_menu()
-    }
--- online_store/shop/admin.py
+++ online_store/shop/admin.py
@@ -1,18 +1,10 @@
 from django.contrib import admin
-from django.core.cache import cache
+
 from .models.product import Product, ProductImage
 from .models.category import Category
 
 
-@admin.action(description="Сбросить кеш меню категорий")
-def clear_category_menu_cache(modeladmin, request, queryset):
-    cache_key = 'category_menu'
-    cache.delete(cache_key)
-    modeladmin.message_user(request, "Кеш меню категорий сброшен.")
-
 admin.site.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    actions = [clear_category_menu_cache]
 
 class ProductInline(admin.StackedInline):
     model = ProductImage
@@ -21,4 +13,3 @@ class ProductInline(admin.StackedInline):
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "icon_preview")
     readonly_fields = ("icon_preview",)
-    actions = [clear_category_menu_cache]
