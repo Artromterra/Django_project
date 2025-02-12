811efb592dfd2d4e691d8f6f98f4eb929e78ee6e
dmitriy <oberon@mail.ru>
2025-02-12 20:25:16 +0300

    Draft version of product characteristic template

--- online_store/shop/urls.py
+++ online_store/shop/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import check_integration_with_frontend
+from .views import check_integration_with_frontend, product_characteristic
 
 app_name = "shop"
 
 # TODO: Remove the check_integration_with_frontend view function
 #  - it is needed to check integration with the base frontend
 urlpatterns = [
-    path("check-frontend/", check_integration_with_frontend, name="check-frontend")
+    path("check-frontend/", check_integration_with_frontend, name="check-frontend"),
+    path("product-characteristic/<int:product_id>/", product_characteristic, name="product-characterictic")
 ]
--- online_store/shop/views.py
+++ online_store/shop/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models.product import Product
+from django.forms.models import model_to_dict
 
 # TODO: Remove the check_integration_with_frontend view function
 #  - it is needed to check integration with the base frontend
 def check_integration_with_frontend(request: HttpRequest) -> HttpResponse:
     return render(request, "base.html")
+
+def product_characteristic(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    characteristics = model_to_dict(product)  # Преобразуем объект в словарь
+    return render(request,
+                  'product_characteristic_template.html',
+                  {'product': product, 'characteristics': characteristics})
