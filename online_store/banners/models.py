fb7686f115c35044d1d1316286dbfca749897381
Антон Рудь <aar260666@gmail.com>
2025-02-13 00:04:00 +0300

    removed the conflict

--- online_store/banners/models.py
+++ online_store/banners/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from shop.models.product import Product
-from shop.models.category import Category
+from products.models import Product
+from categories.models import Category
 
 
 def banner_category_directory_path(
