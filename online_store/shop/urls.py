d82b67614c1ecbc8871e85b06d01ba1e4e5133a3
Dmitriy <oberon@mail.ru>
2025-02-16 21:15:00 +0300

    Html template for product characteristic

--- online_store/shop/urls.py
+++ online_store/shop/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import check_integration_with_frontend, product_characteristic
+from .views import check_integration_with_frontend
+# from .views import product_characteristic
 
 app_name = "shop"
 
@@ -9,5 +10,5 @@ app_name = "shop"
 #  - it is needed to check integration with the base frontend
 urlpatterns = [
     path("check-frontend/", check_integration_with_frontend, name="check-frontend"),
-    path("product-characteristic/<int:product_id>/", product_characteristic, name="product-characterictic")
+    # path("product-characteristic/<int:product_id>/", product_characteristic, name="product-characterictic")
 ]
