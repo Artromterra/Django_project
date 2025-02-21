0c342ecf5e92df768ab88462f1b967e25620cdae
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-21 21:45:44 +0500

    Fix urlpatterns for ReviewsListView and ReviewsCreateView

--- online_store/reviews/urls.py
+++ online_store/reviews/urls.py
@@ -6,6 +6,6 @@ from .views import ReviewsListView, ReviewsCreateView
 app_name = "reviews"
 
 urlpatterns = [
-    path("reviews/", ReviewsListView.as_view(), name="reviews-list"),
-    path("reviews/new/", ReviewsCreateView.as_view(), name="reviews-new"),
+    path("", ReviewsListView.as_view(), name="reviews-list"),
+    path("new/", ReviewsCreateView.as_view(), name="reviews-new"),
 ]
