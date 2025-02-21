28440ec1a4dba1c2fcb7005f0975e6374ae1c5e7
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-21 16:13:19 +0500

    Replace feedback with reviews everywhere; fix ReviewsListView.get_queryset - add select_related for Product;

--- online_store/reviews/views.py
+++ online_store/reviews/views.py
@@ -5,34 +5,38 @@ from django.views.generic import ListView, CreateView
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models.feedbacks import Feedback
+from online_store.shop.models.reviews import Review
+from online_store.shop.models.product import Product
 
 
-class FeedbacksListView(ListView):
-    model = Feedback
-    template_name = "feedback-list.html"
-    context_object_name = "feedbacks"
+class ReviewsListView(ListView):
+    model = Review
+    template_name = "review-list.html"
+    context_object_name = "reviews"
 
     def get_queryset(self):
         limit = self.request.GET.get("limit", settings.DEFAULT_LIMIT_FEEDBACKS)
         product_id = self.request.GET.get("product_id")
-        return (Feedback.objects.filter(Feedback.product.id == product_id)
-                .select_related("User").order_by("-pub_date").all()[:limit])
+        return (Review.objects
+                .select_related("Product").filter(Review.product.id == product_id)
+                .select_related("User").order_by("-created_at").all()[:limit])
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         product_id = self.request.GET.get("product_id")
-        context["num_feedbacks"] = Feedback.objects.filter(Feedback.product.id == product_id).count()
+        context["num_reviews"] = Review.objects.filter(Review.product.id == product_id).count()
         return context
 
 
-class FeedbackCreateView(LoginRequiredMixin, CreateView):
-    model = Feedback
-    fields = ["comment"]
-    template_name = "feedback-create.html"
-    context_object_name = "feedback"
+class ReviewsCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    fields = ["content"]
+    template_name = "review-create.html"
+    context_object_name = "review"
 
     def form_valid(self, form):
+        product_id = self.request.GET.get("product_id")
+        form.instance.product = Product.objects.get(pk=product_id)
         form.instance.user = self.request.user
         form.instance.pub_date = datetime.now()
         return super().form_valid(form)
