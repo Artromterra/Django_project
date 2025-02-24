2d8e3df38627467378f0ab6b45b937216fed337c
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-24 09:14:30 +0500

    Add test test_compiling_report_with_unsuccessful_import

--- online_store/utils/tests/test_email.py
+++ online_store/utils/tests/test_email.py
@@ -1,17 +1,27 @@
 """The module responsible for testing the module email.py."""
 from django.test import TestCase
+from django.conf import settings
 
-from ..email import compile_report, SUCCESS_REPORT_TEXT, REPORT_SUBJECT, FAILURE_REPORT_TEXT
+from ..email import compile_report, SUCCESS_REPORT_TEXT, REPORT_SUBJECT
 
 
 class TestReportText(TestCase):
 
     def setUp(self):
         self.file_path = "example.xlsx"
-        self.log_file_path = "log_example.log"
+        self.log_file_path = settings.BASE_DIR / "utils" / "tests" / "test_logfile.log"
 
     def test_compiling_report_with_successful_import(self):
         """Test compiling report in case successful import."""
         subject, text = compile_report(True, self.file_path, self.log_file_path)
         self.assertEqual(subject, REPORT_SUBJECT.format(status="successful"))
         self.assertEqual(text, SUCCESS_REPORT_TEXT.format(file_path=self.file_path))
+
+    def test_compiling_report_with_unsuccessful_import(self):
+        """Test compiling report in case unsuccessful import."""
+        subject, text = compile_report(False, self.file_path, self.log_file_path)
+        self.assertEqual(subject, REPORT_SUBJECT.format(status="unsuccessful"))
+        self.assertIn("ERROR", text)
+        self.assertIn("WARNING", text)
+        self.assertNotIn("DEBUG", text)
+        self.assertNotIn("INFO", text)
