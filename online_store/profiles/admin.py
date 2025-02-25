fa7d23128b1dc03a51c607e5dd5faa6800ad5359
Роман Лавдоренко <wertuozo@yandex.ru>
2025-02-25 18:29:34 +0300

    add model history view of products in admin for user

--- online_store/profiles/admin.py
+++ online_store/profiles/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
 from .models import User
+from viewed_products.models import ViewedProducts
 
+
+class ViewedProductsAdmin(admin.StackedInline):
+    model = ViewedProducts
+    list_display = ('viewed_at', 'product')
+    ordering = ('-viewed_at', )
+    extra = 0
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -19,3 +26,5 @@ class UserAdmin(admin.ModelAdmin):
     readonly_fields = 'image_preview',
     search_fields = 'username', 'email'
     ordering = 'id',
+
+    inlines = (ViewedProductsAdmin,)
