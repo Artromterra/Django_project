20a021c44247a0d80f0d47342dcdec17cf99bf78
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-03-06 21:59:06 +0500

    Update ReviewFactory

--- online_store/shop/factories/reviews.py
+++ online_store/shop/factories/reviews.py
@@ -8,14 +8,17 @@ from shop.models.reviews import Review
 
 
 class ReviewFactory(factory.django.DjangoModelFactory):
-    """Review factory class."""
+    """
+    Review factory class.
+
+    To create an instance,
+    you must transfer the product and author instances.
+    For example
+    ProductFactory.create(product=product_instance, author=user_instance)
+    """
 
     class Meta:
         model = Review
-        django_get_or_create = (
-            "content",
-            "created_at",
-        )
 
     content = factory.faker.Faker("text")
     created_at = factory.LazyAttribute(lambda now: datetime.datetime.now())
