abdc3b37af21cd448ae7c883d17b0bd3a87755f8
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-04-10 11:54:52 +0500

    Minor fixes

--- online_store/online_store/context_processors.py
+++ online_store/online_store/context_processors.py
@@ -32,6 +32,9 @@ def comparison(request):
     """
     user = request.user if request.user.is_authenticated else None
     session_key = request.session.session_key if not user else None
+    if not session_key:
+        request.session.create()
+        session_key = request.session.session_key
 
     comparison_service = ComparisonService(user=user, session_key=session_key)
 
