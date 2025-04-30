e6de5d84c3c5a8b574f8f7a5bba19503399442aa
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-04-30 18:49:30 +0500

    Rename celery app

--- online_store/online_store/celery.py
+++ online_store/online_store/celery.py
@@ -7,7 +7,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_store.settings')
 
-app = Celery('proj')
+app = Celery('online_store')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
