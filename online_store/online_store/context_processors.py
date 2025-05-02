a64304dceee76e1d601e2cd171688b9864452333
Роман Лавдоренко <wertuozo@yandex.ru>
2025-05-02 13:30:13 +0300

    fixed data in cart logo in header.html

--- online_store/online_store/context_processors.py
+++ online_store/online_store/context_processors.py
@@ -17,12 +17,7 @@ def cart(request):
     Context processor, который добавляет корзину в контекст всех шаблонов.
     """
     cart_service = CartService(request)
-    # cart_items = cart_service.get_cart_items()
     cart_total_price = cart_service.get_cart_total_price()
-    # for item in cart_items:
-    #     discount_service = DiscountService(product=item.product)
-    #     cart_total_price += item.quantity * discount_service.one_product_discount_price()
-    # cart_total_price = sum(item.quantity * item.product.price for item in cart_items)
     cart_total_count = cart_service.get_cart_count()
 
     return {
